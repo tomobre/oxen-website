dc280a0fe35bc2515e3092f99bb04a1b02312e97
tomobre <tomobre@gmail.com>
2023-03-27 16:34:16 +1100

    fix: test csp

--- next.config.js
+++ next.config.js
@@ -8,8 +8,8 @@ const ContentSecurityPolicy = `
     process.env.NODE_ENV == 'development' ? "'unsafe-eval' " : ''
   }'unsafe-inline' *.ctfassets.net *.youtube.com *.twitter.com;
   child-src 'self' *.ctfassets.net *.youtube.com player.vimeo.com *.twitter.com;
-  frame-src 'self' https://optf.ngo/ https://staging.getsession.io/ https://getsession.io/ https://lokinet.org;
-  frame-ancestors 'self' https://optf.ngo/ https://staging.getsession.io/ https://getsession.io/ https://lokinet.org;
+  frame-src 'self' https://optf.ngo/ https://staging.getsession.org/ https://getsession.org/ https://lokinet.org;
+  frame-ancestors 'self' https://optf.ngo/ https://staging.getsession.org/ https://getsession.org/ https://lokinet.org;
   style-src 'self' 'unsafe-inline' *.googleapis.com;
   img-src 'self' blob: data: *.ctfassets.net *.youtube.com *.twitter.com;
   media-src 'self' *.youtube.com;
